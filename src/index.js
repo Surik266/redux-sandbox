13e7a04a845757ed131ea64e94a62a806be39a52
surik266 <surik.pozoyan@gmail.com>
2019-07-29 02:59:40 +0400

    prepared workspace

--- src/index.js
+++ src/index.js
@@ -1,12 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import { createStore } from 'redux'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const reducer = (state = 0, { type } ) => {
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+    switch ( type ) {
+        case 'INC':
+            return state + 1
+        default:
+            return state
+    }
+
+}
+
+const store = createStore(reducer)
+
+console.log(store.getState())
+
+store.dispatch({type: 'INC'})
+
+console.log(store.getState())
